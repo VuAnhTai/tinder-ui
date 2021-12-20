de7866d6c656a6cce85d3d261b5c1867f01bbf3a
taiva <vuanhtai1997@gmail.com>
2021-12-20 23:05:50 +0700

    Create header

---
+++ components/Header/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styles from "./style.module.scss";
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import { IconButton } from '@mui/material';
+export class Header extends Component {
+  render() {
+    return (
+      <div className={styles.header}>
+        <IconButton>
+          <PersonIcon className={styles.header_icon}></PersonIcon>
+        </IconButton>
+        <IconButton>
+          <img className={styles.header_logo} src="logo.svg"></img>
+        </IconButton>
+        <IconButton className={styles.header_logo}>
+          <ForumIcon></ForumIcon>
+        </IconButton>
+      </div>
+    )
+  }
+}
+
+export default Header
